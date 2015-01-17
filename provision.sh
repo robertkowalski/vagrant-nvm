2f79aca42cdb0c31c9a35cc3a57d8eec869e9f6f
Eugen Soloviov <suenot@gmail.com>
2015-01-17 12:26:53 +0400

    fix npm https://github.com/npm/npm/issues/6309, fix installing packages from sudo, fix enabled nvm without a logout/login (fix for me)

--- provision.sh
+++ provision.sh
@@ -4,6 +4,12 @@ sudo apt-get install -y git-core curl
 
 sudo su vagrant -c "wget -qO- https://raw.github.com/creationix/nvm/master/install.sh | sh"
 
+# This enables NVM without a logout/login
 source /home/vagrant/.profile
+source ~/.nvm/nvm.sh
+export NVM_DIR="/home/vagrant/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"
+# 
 nvm install 0.10
 nvm alias default 0.10
+npm -g install npm@2.1.1 # or npm -g install npm@next

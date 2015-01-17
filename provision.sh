6370db7c11e09336d7dfc2d97090bb8ff8abff2a
Eugen Soloviov <suenot@gmail.com>
2015-01-17 12:34:48 +0400

    my work config: express yo bower grunt gulp generator-webapp, cd /vagrant && gulp

--- provision.sh
+++ provision.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
-sudo apt-get install -y git-core curl
+cd /home/vagrant
+
+sudo apt-get update
+sudo apt-get install -y git git-core curl mc
+sudo apt-get -y install build-essential libssl-dev
+sudo apt-get update
 
 sudo su vagrant -c "wget -qO- https://raw.github.com/creationix/nvm/master/install.sh | sh"
-
 # This enables NVM without a logout/login
 source /home/vagrant/.profile
 source ~/.nvm/nvm.sh
@@ -11,5 +15,12 @@ export NVM_DIR="/home/vagrant/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"
 # 
 nvm install 0.10
+nvm use 0.10
 nvm alias default 0.10
-npm -g install npm@2.1.1 # or npm -g install npm@next
+npm -g install npm@2.1.1
+# npm -g install npm@next # alternative fix
+npm install -g express yo bower grunt-cli gulp
+npm install -g generator-webapp
+echo "source ~/.nvm/nvm.sh" >> /home/vagrant/.profile
+echo "cd /vagrant" >> /home/vagrant/.profile
+echo "gulp" >> /home/vagrant/.profile

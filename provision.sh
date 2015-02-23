fbac37da026666aacc253923b9174d898d6899c2
Robert Kowalski <rok@kowalski.gd>
2015-02-24 00:57:39 +0100

    fix nvm not available after initial provisioning
    you had to stop/start the box before

--- provision.sh
+++ provision.sh
@@ -4,6 +4,7 @@ sudo apt-get install -y git-core curl
 
 sudo su vagrant -c "curl https://raw.githubusercontent.com/creationix/nvm/v0.23.3/install.sh | bash"
 
+echo "source /home/vagrant/.nvm/nvm.sh" >> /home/vagrant/.profile
 source /home/vagrant/.profile
 nvm install 0.10
 nvm alias default 0.10

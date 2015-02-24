d949b5221b0e756c382ba99f7315dfa874b76b6d
Robert Kowalski <rok@kowalski.gd>
2015-02-24 01:29:24 +0100

    fix permissions

--- provision.sh
+++ provision.sh
@@ -2,7 +2,7 @@
 
 sudo apt-get install -y git-core curl
 
-sudo su vagrant -c "curl https://raw.githubusercontent.com/creationix/nvm/v0.23.3/install.sh | bash"
+curl https://raw.githubusercontent.com/creationix/nvm/v0.23.3/install.sh | bash
 
 echo "source /home/vagrant/.nvm/nvm.sh" >> /home/vagrant/.profile
 source /home/vagrant/.profile
